Fixed VM computation for CCVA

ccp/portfolio.py
@@ -10,13 +10,14 @@ import numpy as np
 from finance.portfolio import Portfolio
 
 class CCPPortfolio(Portfolio):
-    def __init__(self, matrix_positions, derivatives, prices, exposures):
+    def __init__(self, members_positions_mat, derivatives, prices, exposures):
         # The shape = (m,n) with m the number of CMs, n the number of assets
-        mat = np.matrix(matrix_positions)
+        mat = np.matrix(members_positions_mat)
         for i in range(mat.shape[1]):
             assert np.sum(mat[:, i]) == 0, "The total portfolio composition is not neutral"
             
-        super(CCPPortfolio, self).__init__(matrix_positions, derivatives, prices)
+        ccp_positions = -mat
+        super(CCPPortfolio, self).__init__(ccp_positions, derivatives, prices)
             
         self._exposures_ = np.array(exposures)
                 
@@ -25,7 +26,6 @@ class CCPPortfolio(Portfolio):
     def compute_exposure(self, t, **kwargs):
         tmp = np.zeros((self.cm_number, 1))
         for i, e in enumerate(self._exposures_):
-#            tmp += e(t, self, i, **kwargs)
             tmp += e(t=t, portfolio=self, derivative_index=i, **kwargs)
                 
         return tmp